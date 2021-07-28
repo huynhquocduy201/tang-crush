d879d2dab23e641312d71a099559bdfc0a83c60f
Duy <85848239+huynhquocduy201@users.noreply.github.com>
2021-07-28 23:04:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích tôi <33',
+    btnYes: 'thích duyDZ lâu lắm rồi <33',
     btnNo: 'Không, bạn à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'tôi biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Hết dịch tôi qua đón đi chơi nhoé.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://fb.com' //https://www.facebook.com/messages/t/100003902351169
 }
